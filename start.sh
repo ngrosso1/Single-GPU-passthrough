e2a728e0ba93914a2fe8b77b2ef1a24d4f626d93
ngrosso1 <nicholasjgrosso@protonmail.com>
2025-11-13 00:11:31 -0500

    changed start template to kill lingering apps using nvidia gpu

--- start.sh
+++ start.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
-# Helpful to read output when debugging
 set -x
 
 # Stop display manager
 systemctl stop display-manager.service
-## Uncomment the following line if you use GDM
-#killall gdm-x-session
+
+#Kill any lingering X/Wayland sessions holding the GPU
+killall -q Xorg Xwayland gdm-xsession || true
+
+sudo lsof /dev/nvidia* /dev/dri/* /dev/fb0 2>/dev/null \
+  | awk 'NR>1 {print $2}' | sort -u | tee /tmp/gpu_pids.txt \
+  | xargs -r sudo kill -TERM
+
+sleep 2
+
+sudo lsof /dev/nvidia* /dev/dri/* /dev/fb0 2>/dev/null \
+  | awk 'NR>1 {print $2}' | sort -u | xargs -r sudo kill -9
 
 for i in {1..5}; do
     modprobe -r nvidia_drm && break
     sleep 1
 done
 
-sudo rmmod nvidia_drm
-sudo rmmod nvidia_uvm
 sudo rmmod nvidia_modeset
+sudo rmmod nvidia_uvm
 sudo rmmod nvidia
 
 # Unbind VTconsoles
@@ -24,8 +31,7 @@ echo 0 > /sys/class/vtconsole/vtcon1/bind
 # Unbind EFI-Framebuffer
 echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-# Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
+# Avoid a Race condition by waiting
 sleep 2
 
-# Load VFIO Kernel Module  
-modprobe vfio-pci  
+modprobe vfio-pci
